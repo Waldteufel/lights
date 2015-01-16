a016a6b707945dad96056623ef3cebda6124be0f
Martin Weinelt <mweinelt@gmail.com>
2015-01-16 23:32:59 +0100

    fix pep8

--- artdmx.py
+++ artdmx.py
@@ -17,7 +17,6 @@ HEADER = numpy.dtype([
 
 
 class Client(object):
-
     def __init__(self, length, host, port=6454, universe=0):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.connect((host, port))
@@ -29,7 +28,8 @@ class Client(object):
         self.header['sequence'] = 0
         self.header['universe'] = universe
         self.header['length'] = length
-        self._values = numpy.ndarray(shape=length, dtype='u1', buffer=self.buf, offset=HEADER.itemsize)
+        self._values = numpy.ndarray(shape=length, dtype='u1', buffer=self.buf,
+                                     offset=HEADER.itemsize)
 
     @property
     def values(self):
@@ -48,13 +48,20 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Send an ArtDMX packet')
-    parser.add_argument('values', metavar='V', type=int, nargs='+', help='channel values')
-    parser.add_argument('-u', '--universe', metavar='UNIV', type=lambda x: int(x, 0), default=0, help='universe id (default=0)')
-    parser.add_argument('-s', '--server', metavar='HOST', type=str, default='localhost', help='server (default=localhost)')
-    parser.add_argument('-p', '--port', metavar='PORT', type=int, default=6454, help='udp port (default=6454)')
+    parser.add_argument('values', metavar='V', type=int,
+                        nargs='+', help='channel values')
+    parser.add_argument('-u', '--universe', metavar='UNIV',
+                        type=lambda x: int(x, 0), default=0,
+                        help='universe id (default=0)')
+    parser.add_argument('-s', '--server', metavar='HOST',
+                        type=str, default='localhost',
+                        help='server (default=localhost)')
+    parser.add_argument('-p', '--port', metavar='PORT', type=int,
+                        default=6454, help='udp port (default=6454)')
 
     args = parser.parse_args()
 
-    c = Client(len(args.values), args.server, args.port, universe=args.universe)
+    c = Client(len(args.values), args.server, args.port,
+               universe=args.universe)
     c.values[:] = args.values
     c.push()
