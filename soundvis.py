6a2fb4eeac37676af816caf226cd99db35953a66
Benjamin Richter <br@waldteufel.eu>
2015-01-17 19:51:00 +0100

    make soundvis.py more configurable

--- soundvis.py
+++ soundvis.py
@@ -6,6 +6,19 @@ import numpy as np
 import scipy
 import scipy.signal
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Visualize sound over ArtDMX.')
+parser.add_argument('-u', '--universe', metavar='UNIV', default=0,
+                    type=lambda x: int(x, 0), help='universe id (default=0)')
+parser.add_argument('-s', '--server', metavar='HOST', default='localhost',
+                    type=str, help='server (default=localhost)')
+parser.add_argument('-p', '--port', metavar='PORT', default=6454, type=int,
+                    help='udp port (default=6454)')
+
+args = parser.parse_args()
+
+
 READ_FRAMES = 1764
 RATE = 44100
 
@@ -13,7 +26,7 @@ pa = pyaudio.PyAudio()
 stream = pa.open(format=pyaudio.paInt16, channels=1, rate=RATE,
                  input=True, input_device_index=None,
                  frames_per_buffer=READ_FRAMES)
-dmx = artdmx.Client(75, 'localhost', universe=0x100)
+dmx = artdmx.Client(75, args.server, args.port, universe=args.universe)
 
 buf = np.zeros(dtype='float64', shape=2**15)
 upper = 1e-7
